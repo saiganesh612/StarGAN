651246372cc4e63dee126213447ba41f1e98b7f9
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2023-05-27 23:05:52 +0530

    ready to deploy

--- src/Audio.js
+++ src/Audio.js
@@ -1,10 +1,29 @@
-import Transcript from "./Transcript"
+import { SoligaPaths, LambaniPaths } from "./path"
 
 const DisplayFiles = ({ src }) => {
+    const data = src === "soliga" ? SoligaPaths : LambaniPaths
+
     return (
         <>
-            <h2>{src === "soliga" ? "Soliga TTS Results" : "Lambani TTS Results"}</h2>
-            <Transcript src={src} />
+            <h2>{src === "soliga" ? "Soliga-Kanada StarGAN Results" : "Lambani-Kanada StarGAN Results"}</h2>
+            <div className="mb-4">
+                <div className="row my-3">
+                    <div className="col"><h3>Source</h3></div>
+                    <div className="col"><h3>Target</h3></div>
+                    <div className="col"><h3>Result</h3></div>
+                </div>
+                {
+                    data.source.map((path, index) =>
+                        <div className="mt-4" key={index}>
+                            <div className="row">
+                                <div className="col"><audio src={path} controls /></div>
+                                <div className="col"><audio src={data.target[index]} controls /></div>
+                                <div className="col"><audio src={data.result[index]} controls /></div>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
         </>
     )
 }
--- src/path.js
+++ src/path.js
@@ -1,110 +1,78 @@
 const SoligaPaths = {
-    hifi: [
-        "/assets/Soliga/hifi/2185.wav",
-        "/assets/Soliga/hifi/2277.wav",
-        "/assets/Soliga/hifi/2290.wav",
-        "/assets/Soliga/hifi/4192h.wav",
-        "/assets/Soliga/hifi/4195h.wav",
-        "/assets/Soliga/hifi/4197h.wav",
-        "/assets/Soliga/hifi/4198h.wav"
+    source: [
+        "/assets/out_star_soliga_to_kanada/original_lambani_10.wav",
+        "/assets/out_star_soliga_to_kanada/original_lambani_20.wav",
+        "/assets/out_star_soliga_to_kanada/original_lambani_28.wav",
+        "/assets/out_star_soliga_to_kanada/original_lambani_34.wav",
+        "/assets/out_star_soliga_to_kanada/original_lambani_42.wav",
+        "/assets/out_star_soliga_to_kanada/original_lambani_50.wav",
+        "/assets/out_star_soliga_to_kanada/original_lambani_66.wav",
+        "/assets/out_star_soliga_to_kanada/original_lambani_73.wav",
+        "/assets/out_star_soliga_to_kanada/original_lambani_88.wav",
+        "/assets/out_star_soliga_to_kanada/original_lambani_91.wav"
     ],
-    original: [
-        "/assets/Soliga/original/2185.wav",
-        "/assets/Soliga/original/2277.wav",
-        "/assets/Soliga/original/2290.wav",
-        "/assets/Soliga/original/4192.wav",
-        "/assets/Soliga/original/4195.wav",
-        "/assets/Soliga/original/4197.wav",
-        "/assets/Soliga/original/4198.wav"
+    target: [
+        "/assets/out_star_soliga_to_kanada/p7_10_ref_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_20_ref_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_28_ref_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_34_ref_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_42_ref_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_50_ref_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_66_ref_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_73_ref_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_88_ref_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_91_ref_vocoder.wav"
     ],
-    waveglow: [
-        "/assets/Soliga/waveglow/2185.wav",
-        "/assets/Soliga/waveglow/2277.wav",
-        "/assets/Soliga/waveglow/2290.wav",
-        "/assets/Soliga/waveglow/4192w.wav",
-        "/assets/Soliga/waveglow/4195w.wav",
-        "/assets/Soliga/waveglow/4197w.wav",
-        "/assets/Soliga/waveglow/4198w.wav"
-    ],
-    transcript: [
-        "1.ನೀವುಗ ಏವ  ದಿನವ್ವೆ ಏಳಿಕೆಯಾಗ ಕಾಫಿಯ ಕುಡಿದಿರಾ?",
-        "2.ಅಪ್ಪ ಸಿಂಹೇ ತಿಂಡಿಯ ಹಣಕ ಎತ್ತುರವರೇ.",
-        "3.ಅವಳ ಮೆನೇಲಿ ಎಲ್ಲರುವೆ ಸೇರಿದದು ಬಾರಿ ಬಂಡಾಗಿತ್ತು.",
-        "4.ನಾ ನೆನ್ನೆ ಇರುಳಾಗ ಎರೆಯ ನೋಡೀನೇ .",
-        "5.ನಾ ಅದವ ಮರ್ತಾಗೆ ತೋರಿದ್ದೆ",
-        "6.ನಾ ಮಧ್ಯಕ ತಡೆ ಆಗಬಾರದು",
-        "7.ನಂಗ  ದೇನೋ ಕೆಟ್ಟ ನಾಥ ಬಂತು ."
+    result: [
+        "/assets/out_star_soliga_to_kanada/p7_10_converted_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_20_converted_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_28_converted_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_34_converted_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_42_converted_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_50_converted_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_66_converted_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_73_converted_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_88_converted_vocoder.wav",
+        "/assets/out_star_soliga_to_kanada/p7_91_converted_vocoder.wav",
     ]
 }
 
 const LambaniPaths = {
-    hifi: [
-        "/assets/Lambani/hifi/1h.wav",
-        "/assets/Lambani/hifi/2h.wav",
-        "/assets/Lambani/hifi/3h.wav",
-        "/assets/Lambani/hifi/4h.wav",
-        "/assets/Lambani/hifi/5h.wav",
-        "/assets/Lambani/hifi/6h.wav",
-        "/assets/Lambani/hifi/7h.wav",
-        "/assets/Lambani/hifi/8h.wav",
-        "/assets/Lambani/hifi/9h.wav",
-        "/assets/Lambani/hifi/10h.wav",
-        "/assets/Lambani/hifi/44h.wav",
-        "/assets/Lambani/hifi/52h.wav",
-        "/assets/Lambani/hifi/68h.wav",
-        // "/assets/Lambani/hifi/143h.wav",
-        "/assets/Lambani/hifi/147h.wav"
+    source: [
+        "/assets/out_star_lambani_to_kanada/original_lambani_11.wav",
+        "/assets/out_star_lambani_to_kanada/original_lambani_21.wav",
+        "/assets/out_star_lambani_to_kanada/original_lambani_29.wav",
+        "/assets/out_star_lambani_to_kanada/original_lambani_35.wav",
+        "/assets/out_star_lambani_to_kanada/original_lambani_43.wav",
+        "/assets/out_star_lambani_to_kanada/original_lambani_51.wav",
+        "/assets/out_star_lambani_to_kanada/original_lambani_67.wav",
+        "/assets/out_star_lambani_to_kanada/original_lambani_74.wav",
+        "/assets/out_star_lambani_to_kanada/original_lambani_89.wav",
+        "/assets/out_star_lambani_to_kanada/original_lambani_90.wav"
     ],
-    original: [
-        "/assets/Lambani/original/1.wav",
-        "/assets/Lambani/original/2.wav",
-        "/assets/Lambani/original/3.wav",
-        "/assets/Lambani/original/4.wav",
-        "/assets/Lambani/original/5.wav",
-        "/assets/Lambani/original/6.wav",
-        "/assets/Lambani/original/7.wav",
-        "/assets/Lambani/original/8.wav",
-        "/assets/Lambani/original/9.wav",
-        "/assets/Lambani/original/10.wav",
-        "/assets/Lambani/original/44.wav",
-        "/assets/Lambani/original/52.wav",
-        "/assets/Lambani/original/68.wav",
-        // "/assets/Lambani/original/143.wav",
-        "/assets/Lambani/original/147.wav"
+    target: [
+        "/assets/out_star_lambani_to_kanada/p7_11_ref_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_21_ref_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_29_ref_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_35_ref_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_43_ref_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_51_ref_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_67_ref_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_74_ref_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_89_ref_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_90_ref_vocoder.wav"
     ],
-    waveglow: [
-        "/assets/Lambani/waveglow/1w.wav",
-        "/assets/Lambani/waveglow/2w.wav",
-        "/assets/Lambani/waveglow/3w.wav",
-        "/assets/Lambani/waveglow/4w.wav",
-        "/assets/Lambani/waveglow/5w.wav",
-        "/assets/Lambani/waveglow/6w.wav",
-        "/assets/Lambani/waveglow/7w.wav",
-        "/assets/Lambani/waveglow/8w.wav",
-        "/assets/Lambani/waveglow/9w.wav",
-        "/assets/Lambani/waveglow/10w.wav",
-        "/assets/Lambani/waveglow/44w.wav",
-        "/assets/Lambani/waveglow/52w.wav",
-        "/assets/Lambani/waveglow/68w.wav",
-        // "/assets/Lambani/waveglow/143w.wav",
-        "/assets/Lambani/waveglow/147w.wav"
-    ],
-    transcript: [
-        "1.ಭೇಸಿ ಕಿಚಡೆಮಾಯಿ ರಮ್ಮರಿಚ.",
-        "2.ಬಳದ ಆನ ಗಾಡಿನ ಮಾರ್ದಿನೋಚ.",
-        "3.ಶಿಳ್ಖಳ ವಾಳ ಮಾರೀಚ.",
-        "4.ಮರಮೇಲಿ ಜಕೋಣ ಜಾನ್ವರ್ ನಂದಿಮಾಯಿ ತರಿಚ.",
-        "5.ಅಂಗೋಳೋ ಆರೋಚ್.",
-        "6.ಜಂಗಲೇಮಾಯಿ ಅಂಗಾರ ಲಗ್ಗೋಚ.",
-        "7.ಕಾಲ್ ರಾತ್ ಏಕ ಛೋರಿನ ಮಾರ್ನಾಕೇಚ.",
-        "8.ಏಕ ಲಾಂಬ ವಾಸಳಿ ರ ಲಕಡಿ ಪಡಿಚ.",
-        "9.ಅಪಘಾತೆಮ ಘಣೋ ಲೋಯಿ ದರಗೋಚ.",
-        "10.ಘಣೋ ಖಾಣೋ ಹಾಳ ವೇಗೋಚ.",
-        "11.ಬಾಂಬೆಮಾಯಿ ಗೀಲೋ ಹವಾಮಾನ ರಚ ಪಣ ಡೆಲ್ಲಿ ಮಾಯಿ ಸುಕೋ ಹವಾಮಾನ ರಚ.",
-        "12.ವೋರ ಭೀಯಾ ಘರೇನ್ ಆಯೇರ ಆಂಗ ವೋರ ದೋಸ್ತ್ ದರಗೊತೋ.",
-        "13.ಮುಖ್ಯಸ್ಥ ಘಟನೇರ ಮೂಲ ಕಾರಣ ಕಾಯಿಚ್ ಕೆನ್ ಮಾಲಮ ಕರೆನ್.",
-        // "143.ಛೋರಿಯೋ ಅಂದೂರ ಹಾತೇಪರ ಮದರೇಂಗಿ ಲಗಾಡೇನ್ ಪಸಂದ ಪಡಚ.",
-        "14.ಪಿಯೇನ ಮನ ತಾತೋ ಚಾ ದೋ."
+    result: [
+        "/assets/out_star_lambani_to_kanada/p7_11_converted_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_21_converted_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_29_converted_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_35_converted_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_43_converted_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_51_converted_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_67_converted_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_74_converted_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_89_converted_vocoder.wav",
+        "/assets/out_star_lambani_to_kanada/p7_90_converted_vocoder.wav",
     ]
 }
 
